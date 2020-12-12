54321590fdc0e31eef46faed7c5c69ef34940ab3
KosterovVitaliy <65567933+KosterovVitaliy@users.noreply.github.com>
2020-12-12 19:18:46 +0200

    Add project

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://bipbap.ru/wp-content/uploads/2017/01/004.jpg" className="App-logo" alt="logo" />
+        <p>
+          Кучу времени потратил, нихрена работать реакт не хотел,</p><p> до сих пор скрипты запуска npm и yarn не работают
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Привет, что делать дальше?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
